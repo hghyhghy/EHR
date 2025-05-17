78aed71c35ca705850cc6a8e991d2fcec37f2efb
hghyhghy <sarkarsubham092003@gmail.com>
2025-05-17 21:35:34 +0530

    added the edit endpoint

--- EHR/main/urls.py
+++ EHR/main/urls.py
@@ -2,6 +2,7 @@
 from  django.urls  import path
 from  main.authentication.auth import  register_user,logout_user,login_user
 from main.add_family_member.add_member import add_family_members
+from main.edit_family_members.edit_members import edit_family_members
 urlpatterns = [
     path('register/', register_user, name='register'),
     path('login/', login_user, name='login'),
@@ -9,4 +10,7 @@ urlpatterns = [
 
     # endpoint for adding family member
     path('add-family-members/', add_family_members, name='add_family_member'),
+
+    #endpoint for edit  family members 
+    path('edit-family-member/<int:member_id>/', edit_family_members, name='edit_family_member'),
 ]
