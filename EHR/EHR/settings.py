b23bc73aebdf75863c4a69ab20deb3892e095a6e
hghyhghy <sarkarsubham092003@gmail.com>
2025-05-17 14:03:51 +0530

    removed the default permission classes from settings.py

--- EHR/EHR/settings.py
+++ EHR/EHR/settings.py
@@ -160,7 +160,9 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_ALL_ORIGINS = True
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    # 'DEFAULT_PERMISSION_CLASSES': (
+    #     'rest_framework.permissions.IsAuthenticated',
+    # ),
 }
