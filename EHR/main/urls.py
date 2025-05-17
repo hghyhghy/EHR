64fb012423868de689c1ebda0971ba5bc422a193
hghyhghy <sarkarsubham092003@gmail.com>
2025-05-17 22:23:39 +0530

    added urls.py

--- EHR/main/urls.py
+++ EHR/main/urls.py
@@ -3,6 +3,7 @@ from  django.urls  import path
 from  main.authentication.auth import  register_user,logout_user,login_user
 from main.add_family_member.add_member import add_family_members
 from main.edit_family_members.edit_members import edit_family_members
+from main.delete_family_members.delete_members import delete_family_members
 urlpatterns = [
     path('register/', register_user, name='register'),
     path('login/', login_user, name='login'),
@@ -13,5 +14,9 @@ urlpatterns = [
 
     #endpoint for edit  family members 
     path('edit-family-member/<int:member_id>/', edit_family_members, name='edit_family_member'),
+
+
     # 
+    path('delete-family-member/<int:member_id>/', delete_family_members, name='delete_family_member'),
+
 ]
