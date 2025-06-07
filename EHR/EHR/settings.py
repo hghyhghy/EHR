a8c1ae726eba2813071086c5158cb13da785a7b2
Subham Sarkar <140393712+hghyhghy@users.noreply.github.com>
2025-06-07 09:39:39 +0530

    Update settings.py

--- EHR/EHR/settings.py
+++ EHR/EHR/settings.py
@@ -155,26 +155,26 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # mysql://root:eQoNlGKBBohQUKzjExsEfFRnjildEIgb@yamanote.proxy.rlwy.net:33385/railway
 #mysql://root:@hopper.proxy.rlwy.net:32071/railway
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'postgres',
-#         'USER': 'postgres.efnxobqifzeznmsztpcn',    
-#         'PASSWORD': 'instaily2025',
-#         'HOST':'aws-0-ap-south-1.pooler.supabase.com',  # or IP/remote host like 'db.example.com'
-#         'PORT':'6543',        # default MySQL port
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'EHR',
-        'USER': 'root',
-        'PASSWORD': 'subham0902003@#',
-        'HOST': 'localhost',   # or IP/remote host like 'db.example.com'
-        'PORT': '3306',        # default MySQL port
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres.efnxobqifzeznmsztpcn',    
+        'PASSWORD': 'instaily2025',
+        'HOST':'aws-0-ap-south-1.pooler.supabase.com',  # or IP/remote host like 'db.example.com'
+        'PORT':'6543',        # default MySQL port
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'ehr',
+#         'USER': 'root',
+#         'PASSWORD': 'Babu@2002',
+#         'HOST': 'localhost',   # or IP/remote host like 'db.example.com'
+#         'PORT': '3306',        # default MySQL port
+#     }
+# }
 
 
 CORS_ALLOWED_ORIGINS = [
@@ -235,17 +235,15 @@ LOGGING = {
     },
 }
 
+# # settings.py - Production configuration
+# import os
+# from decouple import config  # pip install python-decouple
 
-# Email configuration
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'  # For Gmail
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ehrad2025@gmail.com'
-EMAIL_HOST_PASSWORD = 'jexn xgek ckus yphy'  # Use app password for Gmail
-DEFAULT_FROM_EMAIL = 'ehrad2025@gmail.com'
-
-#session management
-SESSION_ENGINE = 'django.contrib.sessions.backends.db'
-SESSION_COOKIE_AGE = 86400  # 24 hours
-SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+# # Email configuration using environment variables
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
+# EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
+# EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+# EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+# DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
