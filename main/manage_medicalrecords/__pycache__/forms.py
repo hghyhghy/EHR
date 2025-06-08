a6154a4a480b46b4d970fae4a770bfa7762dd792
hghyhghy <sarkarsubham092003@gmail.com>
2025-06-08 21:03:39 +0530

    added the logout functionality in the medical-records section

--- main/manage_medicalrecords/__pycache__/forms.py
+++
@@ -1,8 +0,0 @@
-
-from  django  import  forms
-from  models import  MedicalRecord
-
-class  MedicalRecordForm(forms.ModelForm):
-    class  Meta:
-        model =  MedicalRecord
-        fields = ['report_file', 'prescription_file', 'medicine_names', 'prescribed_tests']
--- main/throttling.py
+++
@@ -1,8 +0,0 @@
-
-from  rest_framework.throttling import AnonRateThrottle
-
-class  LoginRateThrottle(AnonRateThrottle):
-    scope='login'
-
-class  RegisterRateThrottle(AnonRateThrottle):
-    scope='register'
